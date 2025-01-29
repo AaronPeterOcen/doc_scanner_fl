59e99c0e40cafd4a900f012e712b21ce2002870c
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2025-01-29 12:12:08 +0300

    completed the ui of the app

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -14,9 +14,67 @@ class _HomeScreenState extends State<HomeScreen> {
     return Container(
         padding: EdgeInsets.only(top: 50),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             Card(
-              color: Colors.cyanAccent,
+              color: const Color.fromARGB(255, 12, 209, 209),
+              child: SizedBox(
+                height: 75,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    InkWell(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset("assets/images/scanner-100.png",
+                              width: 35, height: 35),
+                          Text(
+                            'Scan',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ],
+                      ),
+                      onTap: () {},
+                    ),
+                    InkWell(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset("assets/images/scanning-100.png",
+                              width: 35, height: 35),
+                          Text(
+                            "Recognize",
+                            style: TextStyle(color: Colors.white),
+                          )
+                        ],
+                      ),
+                      onTap: () {},
+                    ),
+                    InkWell(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset("assets/images/camera-enhance-100.png",
+                              width: 35, height: 35),
+                          Text(
+                            "Enhance",
+                            style: TextStyle(color: Colors.white),
+                          )
+                        ],
+                      ),
+                      onTap: () {},
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Card(
+                color: const Color.fromARGB(255, 8, 60, 88),
+                child: Container(
+                    height: MediaQuery.of(context).size.height - 290)),
+            Card(
+              color: const Color.fromARGB(255, 12, 209, 209),
               child: SizedBox(
                 height: 100,
                 child: Row(
