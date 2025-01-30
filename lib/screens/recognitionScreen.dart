4cc154004b65891a55922db866e66c6146722f30
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2025-01-30 09:22:04 +0300

    testing out the text recognition functionality

--- lib/screens/recognitionScreen.dart
+++ lib/screens/recognitionScreen.dart
@@ -1,5 +1,11 @@
-import 'dart:io';
+// ignore_for_file: unused_local_variable
 
+import 'dart:io';
+import 'dart:math';
+// import 'com.google.mlkit.vision.text.TextRecognition';
+// import 'com.google.mlkit.vision.text.TextRecognizer';
+// import 'com.google.mlkit.vision.text.latin.TextRecognizerOptions';
+import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
 import 'package:flutter/material.dart';
 
 // ignore: must_be_immutable
@@ -12,6 +18,38 @@ class Recognitionscreen extends StatefulWidget {
 }
 
 class _RecognitionscreenState extends State<Recognitionscreen> {
+  late TextRecognizer textRecognizer;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    textRecognizer = TextRecognizer(script: TextRecognitionScript.latin);
+    doTextRecognition();
+  }
+
+  doTextRecognition() async {
+    InputImage inputImage = InputImage.fromFile(widget.image);
+    final RecognizedText recognizedText =
+        await textRecognizer.processImage(inputImage);
+
+    String text = recognizedText.text;
+    print(text);
+    for (TextBlock block in recognizedText.blocks) {
+      final Rect rect = block.boundingBox;
+      final List<Point<int>> cornerPoints = block.cornerPoints;
+      final String text = block.text;
+      final List<String> languages = block.recognizedLanguages;
+
+      for (TextLine line in block.lines) {
+        // Same getters as TextBlock
+        for (TextElement element in line.elements) {
+          // Same getters as TextBlock
+        }
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
